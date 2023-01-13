f0327a3de056666952f9a0947da53975cb69b8e4
ljog <louis.jog@free.fr>
2023-01-13 09:12:33 +0100

    Sets up animation for welcome section Cleans up code

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-// Create DOM Elements for body and #nav-section
+// Create DOM Elements
 const body = document.getElementsByTagName('body');
 const navSection = document.getElementById('nav-section');
 const welcomeSection = document.getElementById('welcome-section');
 const projectsSection = document.getElementById('projects');
-let contactSection = document.getElementById('contact-section');
+const contactSection = document.getElementById('contact-section');
+const welcomeTextDiv = document.getElementById('welcome-text-div');
+const welcomeText = document.getElementById('welcome-text');
+const jobTitle = document.getElementById('job-title')
+
+// control for end of margin expand
+divUp = false;
+
+//increases margin of welcome-text-div to push the rest of the page slowly
+function marginExpand() {
+    if (i >= 30) {
+    welcomeTextDiv.style.marginBottom = i + 'vh';
+    i+= 0.03;
+    } 
+    // acts as a makeshift timer to delay the animation
+    else {
+    console.log('daccord')
+    i+= 0.04;
+    }
+    // when condition is met, stop and update status
+    if (i >= 40) {
+        divUp = true;
+        clearInterval(intervalID)
+        
+    }
+}
+
+
+
+let i = 0;
+
+let intervalID  = setInterval(marginExpand, 1);
+
+//prevents child element from being displayed before animation end
+function displayText() {
+    if (divUp = true) {
+        welcomeText.style.display = 'inline';
+    }
+}
+
+displayText();
 
 // declares vertical coordinate in viewport "y"
-let y= 0;
+let y = 0;
 
 // Checks vertical coordinate on mouse movement and triggers adequate nav-section animation (down or back up)
 onmousemove = e => {
@@ -27,6 +67,7 @@ onmousemove = e => {
     // triggers
     triggerNavBar();
 }; 
+
 
 
 
