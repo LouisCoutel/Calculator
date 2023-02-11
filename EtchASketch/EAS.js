6106a31f82946eab9db841f3c8392e37660cfa35
ljog <louis.jog@free.fr>
2023-02-11 23:45:51 +0100

    Not working but getting there

--- EtchASketch/EAS.js
+++ EtchASketch/EAS.js
@@ -2,18 +2,87 @@
 const container = document.getElementById("container");
 const colorPalette = document.getElementById("color-palette");
 const colorPicker = document.getElementById("color-picker");
-
+const sizeRange = document.getElementById("size-range");
+const sizeOutput = document.getElementById("size-output");
+const sizeButton = document.getElementById("size-button");
+const allowedSizesByHeight = [0];
+const allowedSizesByWidth = [0];
 var pickedColor = "";
 
+const allowedSizes = [1];
+const minSize = 1;
+var desiredSize;
+
+var rows;
+var cols;
+var grid = document.getElementsByClassName("grid-item");
+
+// calculate grid item size on window load or resize
+window.addEventListener("resize", getAllowedSizes());
+
+//executes the grid creation sequence when button to select size is clicked
+sizeButton.onclick = async function() {
+  await setRowsColsNumber();
+  await makeRows();
+  await setGridItemSize();
+};
+
+//get allowed grid item sizes based on container height and width
+async function getAllowedSizes() {
+  let k = 0;
+  for (let i = 0; i < container.clientHeight; i++) {
+    if (container.clientHeight % i == 0) {
+      allowedSizesByHeight[k] = i;
+      k++;
+    }
+  }
+  let l = 0;
+  for (let i = 0; i < container.clientHeight; i++) {
+    if (container.clientHeight % i == 0) {
+      allowedSizesByWidth[l] = i;
+      l++;
+    }
+  }
+
+  allowedSizesByHeight.forEach((element) => {
+    if (allowedSizesByWidth.includes(element)) {
+      allowedSizes.push(element);
+    }
+  });
+  
+  var maxSize = (allowedSizes.length - 1);
+  sizeRange.setAttribute("max", maxSize);
+  sizeRange.setAttribute("min", minSize);
+  console.log("ça marche");
+}
+
 //Set desired square size based on user input (WIP)
-let desiredSize = 50;
+sizeRange.oninput = function setDesiredSize() {
+  desiredSize = allowedSizes[this.value];
+  sizeOutput.innerHTML = desiredSize
+};
 
 //Set number of rows and cols based on desired square size
-let rows = Math.floor(container.clientHeight / desiredSize);
-let cols = Math.floor(container.clientWidth / desiredSize);
+async function setRowsColsNumber() {
+  rows = Math.floor(container.clientHeight / desiredSize);
+  console.log(rows);
+  cols = Math.floor(container.clientWidth / desiredSize);
+  console.log(cols);
+  console.log("2");
+}
+
+//Assign grid item size
+async function setGridItemSize() {
+  for (let i = 0; i < grid.length; i++) {
+    //grid item size
+    grid[i].style.setProperty("width", desiredSize + "px");
+    grid[i].style.setProperty("height", desiredSize + "px");
+  }
+  console.log("4");
+}
 
 //Fill the container with div elements (grid items)
-function makeRows(rows, cols) {
+async function makeRows() {
   //set the grid rows/cols properties for the container
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
@@ -23,6 +92,9 @@ function makeRows(rows, cols) {
     var cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
   }
+  //Create grid items list
+  grid = container.getElementsByClassName("grid-item");
+  console.log("3");
 }
 
 //create color picking buttons in the color palette
@@ -39,22 +111,19 @@ for (let i = 0; i < 11; i++) {
     let style = getComputedStyle(this);
     pickedColor = style.backgroundColor;
     console.log(pickedColor);
-    assignColor()
+    assignColor();
   });
 }
 
-
-
 //add event listener on color input
-colorPicker.addEventListener("input", function assignCustomColor(event) {
-  pickedColor = event.target.value;
-  assignColor();
-}, false);
-
-makeRows(rows, cols);
-
-//Create grid items list
-const grid = container.getElementsByClassName("grid-item");
+colorPicker.addEventListener(
+  "input",
+  function assignCustomColor(event) {
+    pickedColor = event.target.value;
+    assignColor();
+  },
+  false
+);
 
 //control for user input
 var drawing = false;
@@ -67,13 +136,6 @@ window.addEventListener("mousedown", function () {
 window.addEventListener("mouseup", function () {
   drawing = false;
 });
-
-//Assign grid item size and allow user to draw
-for (let i = 0; i <= grid.length; i++) {
-  //grid item size
-  grid[i].style.setProperty("width", desiredSize + "px");
-  grid[i].style.setProperty("height", desiredSize + "px");
-}
 
 //assign current drawing color to drawing event listeners
 function assignColor() {
