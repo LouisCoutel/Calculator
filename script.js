a8b3bc3a1b7540e7737ebb419e368a207ad7dc02
ljog <louis.jog@free.fr>
2023-04-06 15:34:33 +0200

    add color theme button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const colorToggle = document.getElementById("colorTheme");
+const body = document.getElementById("body");
 
-colorToggle.onClick(function switchTheme() => ) 
+colorToggle.onclick = function switchTheme() {
+  if (body.classList.contains("light")) {
+    body.classList.replace("light", "dark");
+  } else {
+    body.classList.replace("dark", "light");
+  }
+};
