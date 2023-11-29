7fee90b3b7c14ea2a220507c25417102b0fe0072
LouisCoutel <louiscoutel75@gmail.com>
2023-11-29 12:41:04 +0100

    integrating Typescript

--- buttonsClass.ts
+++ buttonsClass.ts
@@ -1,11 +1,19 @@
-function $(id) {
-  return document.getElementById(id)
+function $(id: string) {
+  const returned = document.getElementById(id)
+  const el: HTMLElement = returned as HTMLElement
+  return el
 }
 
 class Buttons {
-  constructor(buttonList) {
-    this.digitsButtons = buttonList.digits.map(el => new DigitButton(el))
-    this.opsButtons = buttonList.operators.map(el => new OperatorButton(el))
+  digitsButtons: Array<any>
+  opsButtons: Array<any>
+  backspaceButton: any
+  equalButton: any
+  floatButton: any
+  clearButton: any
+  constructor(buttonList: any) {
+    this.digitsButtons = buttonList.digits.map((el: string) => new DigitButton(el))
+    this.opsButtons = buttonList.operators.map((el: string) => new OperatorButton(el))
     this.backspaceButton = new BackspaceButton(buttonList.backspace)
     this.equalButton = new EqualButton(buttonList.equal)
     this.floatButton = new FloatButton(buttonList.float)
@@ -13,51 +21,61 @@ class Buttons {
   }
 }
 
+
+
 class Button {
-  constructor(input) {
+  element: HTMLElement
+  value: string
+
+  constructor(input: string) {
     this.element = $(input)
     this.value = input
   }
 }
 
 class DigitButton extends Button {
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "digit"
   }
 }
 
 class OperatorButton extends Button {
-  // display operator with spaces on either side
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "operator"
   }
 }
 
 class FloatButton extends Button {
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "float"
   }
 }
 
 class BackspaceButton extends Button {
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "backspace"
   }
 };
 
 class EqualButton extends Button {
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "equal"
   }
 }
 
 class ClearButton extends Button {
-  constructor(input) {
+  type: string
+  constructor(input: string) {
     super(input)
     this.type = "clear"
   }
--- engineClass.js
+++ engineClass.js
@@ -1,9 +1,9 @@
 class Engine {
-    #numbers
-    #operators
-    #counter
-    #opCounter
-    #lastEntered
+    #numbers: Array
+    #operators: Array
+    #counter: Number
+    #opCounter: Number
+    #lastEntered: String
 
     constructor() {
         this.#numbers = [];
@@ -13,8 +13,8 @@ class Engine {
         this.#lastEntered = undefined;
     }
 
-    add(a, b) {
-        let result = a + b;
+    add(a: Number, b: Number) {
+        const result: Number = a + b;
         if (result != 0) {
             return result;
         } else {
@@ -22,8 +22,8 @@ class Engine {
         }
     };
 
-    subtract(a, b) {
-        let result = a - b;
+    subtract(a: Number, b: Number) {
+        const result: Number = a - b;
         if (result != 0) {
             return result;
         } else {
@@ -31,8 +31,8 @@ class Engine {
         }
     };
 
-    multiply(a, b) {
-        let result = a * b;
+    multiply(a: Number, b: Number) {
+        const result: Number = a * b;
         if (result != 0) {
             return result;
         } else {
@@ -40,8 +40,8 @@ class Engine {
         }
     };
 
-    divide(a, b) {
-        let result = a / b;
+    divide(a: Number, b: Number) {
+        const result: Number = a / b;
         if (result != 0) {
             return result;
         } else {
@@ -66,19 +66,19 @@ class Engine {
     }
 
 
-    set lastEntered(value) {
+    set lastEntered(value: String) {
         this.#lastEntered = value
     }
-    set opCounter(value) {
+    set opCounter(value: String) {
         this.#opCounter = value
     }
-    set counter(value) {
+    set counter(value: String) {
         this.#counter = value
     }
-    set operators(value) {
+    set operators(value: String) {
         this.#operators = value
     }
-    set numbers(value) {
+    set numbers(value: String) {
         this.#numbers = value
     }
 
@@ -96,15 +96,15 @@ class Engine {
         return (this.numbers[0] * 100000) / 100000
     }
 
-    #concatToNumbers(input) {
+    #concatToNumbers(input: String) {
         this.#numbers[this.counter] += input
     }
 
-    #initNumbers(input) {
+    #initNumbers(input: String) {
         this.#numbers[this.counter] = input
     }
 
-    setNumber(input) {
+    setNumber(input: String) {
         if (this.numbers[this.counter] != undefined) {
             this.#concatToNumbers(input)
         } else {
@@ -112,7 +112,7 @@ class Engine {
         }
     }
 
-    setOperator(input) {
+    setOperator(input: String) {
         this.operators.push(input)
     }
 
@@ -124,7 +124,7 @@ class Engine {
         }
     }
 
-    setLast(value) {
+    setLast(value: String) {
         this.lastEntered = value
         console.log(this.lastEntered)
     }
@@ -184,13 +184,13 @@ class Engine {
     }
 
     sumOperation() {
-        let result = this.add(this.numbers[this.opCounter], this.numbers[this.opCounter + 1]);
+        const result: Number = this.add(this.numbers[this.opCounter], this.numbers[this.opCounter + 1]);
         this.numbers.splice(this.opCounter, 2, result);
         this.operators.splice(this.opCounter, 1);
     }
 
     subOperation() {
-        let result = this.subtract(
+        const result: Number = this.subtract(
             this.numbers[this.opCounter],
             this.numbers[this.opCounter + 1]
         )
@@ -200,7 +200,7 @@ class Engine {
 
     divOperation() {
         if (this.numbers[1] != 0) {
-            let result = this.divide(
+            const result: Number = this.divide(
                 this.numbers[this.opCounter],
                 this.numbers[this.opCounter + 1]
             );
@@ -212,7 +212,7 @@ class Engine {
     }
 
     multOperation() {
-        let result = this.multiply(
+        const result: Number = this.multiply(
             this.numbers[this.opCounter],
             this.numbers[this.opCounter + 1]
         );
@@ -222,7 +222,7 @@ class Engine {
 
     toActualNumbers() {
         this.#numbers = this.#numbers.map(el => {
-            return (Math.round(parseFloat(el) * 100000) / 100000);
+            return (Math.round(parseFloat(el: Number) * 100000) / 100000);
         });
     }
 
@@ -245,4 +245,4 @@ class Engine {
 
 
 
-export default Engine
+export default class Engine
