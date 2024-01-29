b7195f3583aa9e5f2421218356e46e76a77da473
LouisCoutel <louiscoutel75@gmail.com>
2024-01-29 14:38:32 +0100

    Squashed 'MTT/' changes from fc4095b9..91f0d4e7
    91f0d4e7 Create eslint.yml
    
    git-subtree-dir: MTT
    git-subtree-split: 91f0d4e71eaaf486e24a0d4d450cfd357266a045

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio - LC
-
-This is my personal portfolio. Initially built in vanilla JS/CSS, it's been revamped with Vue3 and Tailwindcss in order to scale appropriately as more projects and features are added.
--- src/atoms/SendButton.vue
+++
@@ -1,6 +0,0 @@
-<script setup></script>
-
-<template>
-  <input type="submit" id="submit" name="submit" value="ENVOYER"
-    class="contact_form-submit rounded-lg bg-gray-200 shadow-gray-400 shadow-sm text-eerieBlack hover:bg-utOrange hover:text-white active:scale-x-95 duration-500 px-1 py-[2px] font-display text-base tallmd:text-lg font-bold leading-none tallmd:leading-tight text-center transition-all" />
-</template>
